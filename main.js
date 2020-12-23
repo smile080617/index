747b8b9ba6e7d715cd35c3d2a529fd4c405d1d5e
smile080617 <76514654+smile080617@users.noreply.github.com>
2020-12-24 00:50:28 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function showMobileMenu(){
+    let MobileMenu=document.querySelector("#mobile-menu");
+    MobileMenu.style.display="block";
+}
+function closeMobileMenu(){
+    let MobileMenu=document.querySelector("#mobile-menu");
+    MobileMenu.style.display="none";
+}
